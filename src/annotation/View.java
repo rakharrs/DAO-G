d0d3dd706451426cb8883e3900f4c743d648e1e6
rakharrs <rodyox.yoann@gmail.com>
2023-01-22 09:22:58 +0300

    Initial commit

---
+++ src/annotation/View.java
@@ -0,0 +1,12 @@
+package model.dbObject.annot;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface View {
+    String name();
+}
